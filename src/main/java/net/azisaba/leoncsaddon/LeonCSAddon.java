b525eeb3e056ad305e666e2b2bdce9374a010791
matsu1213 <matsu1213mgal@gmail.com>
2024-06-19 19:00:02 +0900

    Initial commit

---
+++ src/main/java/net/azisaba/leoncsaddon/LeonCSAddon.java
@@ -0,0 +1,17 @@
+package net.azisaba.leoncsaddon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LeonCSAddon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/net/azisaba/leoncsaddon/WeaponCustomDamageListener.java
@@ -0,0 +1,2 @@
+package net.azisaba.leoncsaddon;public class WeaponCustomDamgeListener {
+}
