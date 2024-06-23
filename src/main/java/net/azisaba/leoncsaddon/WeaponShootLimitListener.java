d5a66a31e1db013661244057fd7bb4af244ee5ec
matsu1213 <matsu1213mgal@gmail.com>
2024-06-23 11:27:19 +0900

    shootlimitlandmine

--- src/main/java/net/azisaba/leoncsaddon/WeaponShootLimitListener.java
+++ src/main/java/net/azisaba/leoncsaddon/WeaponShootLimitListener.java
@@ -4,8 +4,10 @@ import com.shampaggon.crackshot.CSUtility;
 import com.shampaggon.crackshot.events.WeaponPlaceMineEvent;
 import com.shampaggon.crackshot.events.WeaponPreShootEvent;
 import com.shampaggon.crackshot.events.WeaponPrepareShootEvent;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Collections;
@@ -32,22 +34,30 @@ public class WeaponShootLimitListener implements Listener {
     }
 
     @EventHandler
-    public void onPlaceLandmine(WeaponPlaceMineEvent e){
-        WeaponConfigData data = LeonCSAddon.INSTANCE.getWeaponConfig().getWeaponConfigData(e.getWeaponTitle());
-        if(data != null && !data.isMain){
-            for(int i = 0; i < 9; i++){
-                ItemStack item = e.getPlayer().getInventory().getItem(i);
-                String weaponTitle = new CSUtility().getWeaponTitle(item);
-                if(weaponTitle != null){
-                    WeaponConfigData otherData = LeonCSAddon.INSTANCE.getWeaponConfig().getWeaponConfigData(weaponTitle);
-                    if(otherData != null && otherData.isMain){
-                        if(data.requirements != null && otherData.type != null && Collections.disjoint(data.requirements, otherData.type)){
-                            e.getMine().remove();
+    public void onDrop(ItemSpawnEvent e){
+        if(e.getEntity().getPickupDelay() != 24000) return;
+
+        for(Player player: e.getEntity().getWorld().getPlayers()){
+            if(e.getEntity().getLocation().equals(player.getEyeLocation())){
+                WeaponConfigData data = LeonCSAddon.INSTANCE.getWeaponConfig().getWeaponConfigData(new CSUtility().getWeaponTitle(player.getInventory().getItemInMainHand()));
+                if(data != null && !data.isMain){
+                    for(int i = 0; i < 9; i++){
+                        ItemStack item = player.getInventory().getItem(i);
+                        String weaponTitle = new CSUtility().getWeaponTitle(item);
+                        if(weaponTitle != null){
+                            WeaponConfigData otherData = LeonCSAddon.INSTANCE.getWeaponConfig().getWeaponConfigData(weaponTitle);
+                            if(otherData != null && otherData.isMain){
+                                if(data.requirements != null && otherData.type != null && Collections.disjoint(data.requirements, otherData.type)){
+                                    e.setCancelled(true);
+                                }
+                            }
                         }
                     }
                 }
             }
         }
+
+
     }
 
 }
