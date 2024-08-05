a19cc6d72293aacc31108274286c5d2911a94c3c
matsu1213 <matsu1213mgal@gmail.com>
2024-08-05 12:37:43 +0900

    rewrite

--- src/main/java/net/azisaba/leoncsaddon/WeaponPotionListener.java
+++ src/main/java/net/azisaba/leoncsaddon/WeaponPotionListener.java
@@ -1,25 +1,34 @@
 package net.azisaba.leoncsaddon;
 
 import com.shampaggon.crackshot.events.WeaponDamageEntityEvent;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Egg;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.metadata.MetadataValue;
 import org.bukkit.potion.PotionEffect;
 
 public class WeaponPotionListener implements Listener {
 
     @EventHandler
-    public void onDamage(WeaponDamageEntityEvent e){
-        if(!(e.getVictim() instanceof Player)) return;
+    public void onDamage(ProjectileHitEvent e){
+        if(!(e.getHitEntity() instanceof Player)) return;
+        if(!(e.getEntity().getShooter() instanceof Player)) return;
+        if(!(e.getEntity() instanceof Snowball) && !(e.getEntity() instanceof Egg) && !(e.getEntity() instanceof Arrow)) return;
+        if(!e.getEntity().hasMetadata("projParentNode")) return;
 
-        WeaponConfigData data = LeonCSAddon.INSTANCE.getWeaponConfig().getWeaponConfigData(e.getWeaponTitle());
+        WeaponConfigData data = LeonCSAddon.INSTANCE.getWeaponConfig().getWeaponConfigData(((MetadataValue)e.getEntity().getMetadata("projParentNode").get(0)).asString());
 
         if(data == null) return;
 
-        Player victimPlayer = ((Player) e.getVictim());
+        Player shooter = ((Player)e.getEntity().getShooter());
+        Player victimPlayer = ((Player) e.getHitEntity());
 
-        if(e.getPlayer().getScoreboard().getEntryTeam(e.getPlayer().getName())
-                == victimPlayer.getScoreboard().getEntryTeam(e.getVictim().getName())){
+        if(shooter.getScoreboard().getEntryTeam(shooter.getName())
+                == victimPlayer.getScoreboard().getEntryTeam(e.getHitEntity().getName())){
             for(PotionEffect effect: data.allyPotion){
                 victimPlayer.removePotionEffect(effect.getType());
                 victimPlayer.addPotionEffect(effect);
