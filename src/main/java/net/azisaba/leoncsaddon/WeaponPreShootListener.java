4ee509e59044840b04b709899bced8c0bdcdd853
miyabi0333 <57523862+Arisa9006@users.noreply.github.com>
2025-08-06 23:23:32 +0900

    Update WeaponPreShootListener.java

--- src/main/java/net/azisaba/leoncsaddon/WeaponPreShootListener.java
+++ src/main/java/net/azisaba/leoncsaddon/WeaponPreShootListener.java
@@ -1,18 +1,27 @@
 package net.azisaba.leoncsaddon;
 
+import com.destroystokyo.paper.event.player.PlayerJumpEvent;
 import com.shampaggon.crackshot.events.WeaponPreShootEvent;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static net.azisaba.leoncsaddon.LeonCSAddon.INSTANCE;
 
 /**
  * 射撃時の干渉を行う
  * @author AburaAgeTarou
  */
 public class WeaponPreShootListener implements Listener {
-
+	private final Map<UUID, Long> jumpTimestamps = new HashMap<>();
 	/**
 	 * 射撃時処理
 	 * @param event 射撃時イベント
@@ -21,6 +30,11 @@ public class WeaponPreShootListener implements Listener {
 	public void onWeaponPreShoot(WeaponPreShootEvent event) {
 		// 拡散倍率にキャップを適用する
 		double spread = event.getBulletSpread();
+		WeaponConfigData data = INSTANCE.getWeaponConfig().getWeaponConfigData(event.getWeaponTitle());
+		if(data.type.contains("sr") && jumpTimestamps.containsKey(event.getPlayer().getUniqueId())){
+			long elapsedMillis = System.currentTimeMillis() - jumpTimestamps.get(event.getPlayer().getUniqueId());
+			spread += BigDecimal.valueOf((double) elapsedMillis / 1000).setScale(3, RoundingMode.HALF_UP).doubleValue();
+		}
 		spread = capSpread(event.getPlayer(), spread);
 		event.setBulletSpread(spread);
 	}
@@ -50,5 +64,27 @@ public class WeaponPreShootListener implements Listener {
 		// 拡散倍率とキャップの小さい方を採用する
 		BigDecimal spreadBD = new BigDecimal(spread);
 		return spreadBD.min(capBD).doubleValue();
+	}
+	/**
+	 * ジャンプした判定
+	 */
+	@EventHandler
+	public void onJump(PlayerJumpEvent event) {
+		Player player = event.getPlayer();
+		jumpTimestamps.put(player.getUniqueId(), System.currentTimeMillis());
+	}
+
+	@EventHandler
+	public void onMove(PlayerMoveEvent event) {
+		Player player = event.getPlayer();
+		UUID uuid = player.getUniqueId();
+
+		// 追跡中のプレイヤーのみチェック
+		if (!jumpTimestamps.containsKey(uuid)) return;
+
+		// 地面に着いたら
+		if (player.isOnGround()) {
+			jumpTimestamps.remove(uuid);
+		}
 	}
 }
